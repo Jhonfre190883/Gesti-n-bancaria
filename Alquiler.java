97c368eebb98b079de227a5806e6a007ed5f0ab1
Jhonfre190883 <32915671+Jhonfre190883@users.noreply.github.com>
2017-10-18 18:29:22 -0500

    Add files via upload

---
+++ Alquiler.java
@@ -0,0 +1,12 @@
+package alquiler;
+
+public class Alquiler 
+{
+
+     public static void main(String[] args) 
+     {
+        AlquilerInterfaz eu=new AlquilerInterfaz() ;
+        eu.setVisible(true); 
+     }
+    
+}
